43645256945f1b0ac852cbf1d270478cec5a5617
Prashun Chitrakar <pchitkr@gmail.com>
2020-05-15 13:17:26 +0545

    Added comparision operators

--- Day1/operators.py
+++ Day1/operators.py
@@ -1,22 +1,31 @@
 # This file contains basic python operators
 
 # Addition
-print("Addition ", 6+10)
+print("Addition:", 6+10)
 
 # Subraction
-print("Subraction ", 5-7)
+print("Subraction:", 5-7)
 
 # Multiplication
-print("Multplication ", 5*10)
+print("Multplication:", 5*10)
 
 # Division
-print("Division ", 9/2)
+print("Division:", 9/2)
 
 # Floor Division
-print("Floor Division ", 9//2)
+print("Floor Division:", 9//2)
 
 # Modulo Division
-print("Modulo Division ", 5%2)
+print("Modulo Division:", 5%2)
 
 # Exponent
-print("Exponent ", 2**16)
+print("Exponent:", 2**16)
+
+
+# Comparision
+print("Equality:", 1==1)
+print("Inequality:", 1!=1)
+print("Greater Than:", 2>1)
+print("Greater Than Equals To:", 5>=5)
+print("Less Than:", 1<2)
+print("Less Than Equals To:", 5<=10)
