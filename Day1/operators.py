a7c658a17755881ecc89064f50aa1a15982549ac
Prashun Chitrakar <pchitkr@gmail.com>
2020-05-15 13:03:01 +0545

    Added programs done in Day 1

---
+++ Day1/operators.py
@@ -0,0 +1,22 @@
+# This file contains basic python operators
+
+# Addition
+print("Addition ", 6+10)
+
+# Subraction
+print("Subraction ", 5-7)
+
+# Multiplication
+print("Multplication ", 5*10)
+
+# Division
+print("Division ", 9/2)
+
+# Floor Division
+print("Floor Division ", 9//2)
+
+# Modulo Division
+print("Modulo Division ", 5%2)
+
+# Exponent
+print("Exponent ", 2**16)
